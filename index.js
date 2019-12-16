e8a864248240aa6723a7f080f5481e137690cda4
NkanyisoSigwaza <38808434+NkanyisoSigwaza@users.noreply.github.com>
2019-12-16 17:14:54 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express  =require("express");
 
+router = require("./routes/auth-routes");
+
 const app = express();
 
 // set up view engine
 
 app.set("view engine","ejs");
+// set up routes
+
+app.use("/auth",router);
+
 
 app.get('/',function(req,res){
   res.render("home");
 });
+
+
 
 app.listen(3000,function(){
   console.log("Listening to port 3000");
