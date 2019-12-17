97622f824bc40fe18e0e8f843144ea7ffa0f4395
NkanyisoSigwaza <38808434+NkanyisoSigwaza@users.noreply.github.com>
2019-12-17 08:28:15 +0200

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+// add this file to .gitignore
+
+module.exports = {
+  google:{
+    clientID:"853363681762-lch6hdrgktiumjgbf8a13upbq77comf3.apps.googleusercontent.com",
+    clientSecret:"pIy7kM88IOkfhhtzCvwFdivH"
+  },
+  mongodb:{
+    dbURI:"mongodb+srv://Nkah:ssssssst@cluster0-rn4rk.mongodb.net/test?retryWrites=true&w=majority",
+  }
+};
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,10 +1,39 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
+const keys = require("./keys");
 
+const User = require("../models/user-model")
 passport.use(new GoogleStrategy({
   // options for the google strat
-}),
-fuction(){
+  callbackURL:"/auth/google/redirect",
+  clientID:keys.google.clientID,
+  clientSecret:keys.google.clientSecret
+},
+function(accessToken,refreshToken,profile,done){
+  //accessToken retrieved from google to alter users profile
+  //refreshToken refreshes accessToken expires after timeout
+
+
   //passport call back function
+
+  console.log("passport callback function fired");
+
+  // check if user already exists in db
+  User.findOne({googleID:profile.id}).then(function(currentUser){
+    if (currentUser){
+      // already have a user
+      console.log("User is"+currentUser);
+    }
+    else{
+      // if not create user in db
+      new User({
+        username:profile.displayName,
+        googleID:profile.id
+      }).save().then(function(newUser){
+        console.log("new user creater:"+ newUser);
+      });
+    }
+  });
+
 }
-);
+));
