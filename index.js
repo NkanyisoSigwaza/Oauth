8f3c305c355e6d533155bb16fc55d148e75e3b9d
NkanyisoSigwaza <38808434+NkanyisoSigwaza@users.noreply.github.com>
2019-12-17 08:26:27 +0200

    Add files via upload
    connected to online db and allows adding of users without duplicates

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const express  =require("express");
 
 router = require("./routes/auth-routes");
 
+const passportSetup = require("./config/passport-setup");
+
+const mongoose = require("mongoose");
+
+const keys = require("./config/keys");
 const app = express();
 
 // set up view engine
 
 app.set("view engine","ejs");
+
+// connect to mongodb
+mongoose.connect(keys.mongodb.dbURI,function(){
+  console.log("connected to mongodb");
+});
+
 // set up routes
 
 app.use("/auth",router);
